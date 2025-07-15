3868aaeacc7518c35eaed6ecc6699e528cd7b7a5
Proshun Saha <sahaproshun135@gmail.com>
2025-07-14 20:27:24 -0500

    Updated the model.py file

--- model.py
+++ model.py
@@ -1,34 +1,18 @@
-# from torchvision import models, transforms
-# import torch
-# from PIL import Image
-
-#Loading pretrained model and replace final layer
-# model = models.resnet50(pretrained = True)
-# model.fc = torch.nn.Linear(model.fc.in_features, 2)
-# checkpoint = torch.load('resnet50-0676ba61.pth', map_location='cpu')
-# model.load_state_dict(checkpoint)
-# model.eval()
-
 from torchvision import models, transforms
 from torchvision.models import resnet50, ResNet50_Weights
-
 import torch
 import torch.nn as nn
 from PIL import Image
+
 # 1) Build the *full* ResNet (with 1000-way head)
 model = resnet50(weights=ResNet50_Weights.IMAGENET1K_V1)
 
 # 2) Load checkpoint into a dict
 state = torch.load('resnet50-0676ba61.pth', map_location='cpu')
 
-# 3) Remove the classifier weights from that dict
-state.pop('fc.weight', None)
-state.pop('fc.bias',   None)
+model.load_state_dict(state)
 
-# 4) Load the rest (strict=False lets you skip missing keys)
-model.load_state_dict(state, strict=False)
-
-# 5) Now swap in your 2-way head
+# 3) Now swap in your 2-way head
 model.fc = nn.Linear(model.fc.in_features, 2)
 
 model.eval()
@@ -48,7 +32,7 @@ preprocess = transforms.Compose([
         std= [0.229, 0.224, 0.225])
 ])
 
-labels = {0: 'cartoon', 1: 'anime'}
+labels = {0: 'anime', 1: 'cartoon'}
 
 def predict(image: Image.Image) -> dict:
 
